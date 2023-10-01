57d1070d33398ccd022527e8aa7411e2e5bb20f7
olamiji-8 <olamijisotomi@gmail.com>
2023-10-01 22:13:33 +0100

    update

---
+++ config/dbconnect.js
@@ -0,0 +1,11 @@
+const {mongoose} = require("mongoose")
+
+const dbConnect = ()=>{
+  try {
+    const conn = mongoose.connect("mongodb+srv://olapelu:3214321@cluster0.wstltxa.mongodb.net/?retryWrites=true&w=majority");
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database error");
+  }
+}
+module.exports = dbConnect();
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const dbconnect = require("./config/dbconnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000
 
+dbconnect();
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
