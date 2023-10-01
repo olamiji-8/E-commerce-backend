530aa7d561afae430eb094113abce44e8c14c0e6
olamiji-8 <olamijisotomi@gmail.com>
2023-10-01 22:25:27 +0100

    update the code

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const dbconnect = require("./config/dbconnect");
+const dbConnect = require("./config/dbconnect");
+
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000
+dbConnect();
 
-dbconnect();
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
