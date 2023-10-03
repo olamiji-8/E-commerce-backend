498a9feadb8b1344f27968ddde48ae70cc6c7f62
olamiji-8 <olamijisotomi@gmail.com>
2023-10-03 17:24:19 +0100

    update

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,8 +1,8 @@
-const {mongoose} = require("mongoose")
+const {mongoose} = require("mongoose");
 
 const dbConnect = ()=>{
   try {
-    const conn = mongoose.connect("mongodb+srv://olapelu:3214321@cluster0.wstltxa.mongodb.net/?retryWrites=true&w=majority");
+    const conn = mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Database error");
