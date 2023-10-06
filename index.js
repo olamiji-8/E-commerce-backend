9b46e3b7003942278cf66e315c1a850a981c2444
olamiji-8 <olamijisotomi@gmail.com>
2023-10-06 23:31:45 +0100

    update the code

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dbConnect = require("./config/dbconnect");
+const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
 
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000
-dbConnect();
 
+dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use("/api/user",authRouter);
+app.post("/", (req, res)=>{
+res.json({'firstname': 'ola'})    
+});
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
+
